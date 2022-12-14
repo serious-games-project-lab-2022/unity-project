7949824505dc093513880d374bc2abe3c5239d68
Mario Nowak <m.now@protonmail.com>
2022-12-14 08:18:23 +0100

    Add configuration draft

---
+++ Assets/Minigame/ShapeMinigame/ShapeMinigame.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ShapeMinigame : Minigame
+{
+    public List<MinigameShape> shapePrefabs;
+
+    void GenerateConfiguration()
+    {
+        var randomNumberGenerator = new System.Random();
+        // Shuffle prefabs
+        var shuffledPrefabs = shapePrefabs.OrderBy(item => randomNumberGenerator.Next());
+        var absolutePositions = new List<Vector2Int>() {};
+
+        foreach (var shapePrefab in shuffledPrefabs)
+        {
+            if (absolutePositions.Count == 0)
+            {
+                // The first part gets the absolute position (0, 0)
+                absolutePositions.Add(Vector2Int.zero);
+            }
+
+            // Pick random side
+
+            // Pick random coordinate perpendicular to side normal
+
+            // Find closest coordinate parallel to side normal
+
+            // Save coordinate
+
+            // Add shape to configuration
+        }
+        // Compute relative positions between all 
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
