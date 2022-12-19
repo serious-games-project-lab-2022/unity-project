d83707913f96f88f3046049748ce59e3b1a93e8b
fredelberger <facebook.fredelberger@arcor.de>
2022-12-19 18:31:31 +0100

    Refactor minigame

--- Assets/Minigame/Minigame.cs
+++ Assets/Minigame/Minigame.cs
@@ -23,7 +23,6 @@ public class Minigame : MonoBehaviour
        minigameShapeController = FindObjectOfType<MinigameShapeController>();
        minigameShapeController.SetCanMoveShapes(true);
        secondsLeftToSolve = secondsToSolve;
-       Debug.Log("Minigame start");
        timerBar.fillAmount = 1f; // Sets the timerbar to full
     }
 
@@ -31,7 +30,7 @@ public class Minigame : MonoBehaviour
     virtual protected void Update()
     {
         secondsLeftToSolve-= Time.deltaTime;
-        timerBar.fillAmount = secondsLeftToSolve / secondsToSolve; // Makes the timerbar deplete over time
+        depleteTimerBar();
         if(secondsLeftToSolve<=0)
         {
             if (OnMinigameOver!= null)
@@ -39,7 +38,12 @@ public class Minigame : MonoBehaviour
                 OnMinigameOver(); // Trigger MinigameOver Event
             }
             minigameShapeController.SetCanMoveShapes(false);
-            Debug.Log("Time's UP!");
+            
         }
+    }
+
+    private void depleteTimerBar()
+    {
+        timerBar.fillAmount = secondsLeftToSolve / secondsToSolve;
     }
 }
