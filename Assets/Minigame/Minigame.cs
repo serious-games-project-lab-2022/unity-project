67e9e284591610688d91899f3c2f109916a07d83
fredelberger <facebook.fredelberger@arcor.de>
2022-12-23 20:42:41 +0100

    Add TimerBar (#14)
    * Add TimerBar
    
    * Added CanMoveShapes Bool
    
    When the time runs out, shapes cannot be moved anymore.
    
    * Refactor minigame
    
    * Add minor improvements to minigame
    
    * Bind timer bar UI to minigame
    
    Co-authored-by: Mario Nowak <m.now@protonmail.com>

--- Assets/Minigame/Minigame.cs
+++ Assets/Minigame/Minigame.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Minigame : MonoBehaviour
 {
+    [SerializeField] private float secondsToSolve = 30f;
+    private float secondsLeftToSolve;
+    
+    [SerializeField] protected Image timerBar;
+
+    [SerializeField] private MinigameShapeController minigameShapeController;
+
+    public delegate void MinigameOver();
+    public static event MinigameOver OnMinigameOver = delegate { };
+    
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    virtual protected void Start()
+    { 
+       minigameShapeController.SetCanMoveShapes(true);
+       secondsLeftToSolve = secondsToSolve;
+       // Set the timer bar to full
+       timerBar.fillAmount = 1f;
     }
 
     // Update is called once per frame
-    void Update()
+    virtual protected void Update()
     {
-        
+        if (secondsLeftToSolve > 0)
+        {
+            depleteTimerBar();
+            if (secondsLeftToSolve < 0)
+            {
+                OnMinigameOver();
+                minigameShapeController.SetCanMoveShapes(false);
+                secondsLeftToSolve = 0;
+            }
+        }
+    }
+
+    private void depleteTimerBar()
+    {
+        secondsLeftToSolve -= Time.deltaTime;
+        timerBar.fillAmount = secondsLeftToSolve / secondsToSolve;
     }
 }
