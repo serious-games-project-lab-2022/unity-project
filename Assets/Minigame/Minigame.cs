0c7010a13fdd609ef5270409caf231ac46cfa283
Mario Nowak <m.now@protonmail.com>
2022-12-23 20:37:30 +0100

    Add minor improvements to minigame

--- Assets/Minigame/Minigame.cs
+++ Assets/Minigame/Minigame.cs
@@ -6,44 +6,42 @@ using UnityEngine.UI;
 public class Minigame : MonoBehaviour
 {
     [SerializeField] private float secondsToSolve = 30f;
-
     private float secondsLeftToSolve;
     
     [SerializeField] protected Image timerBar;
 
+    [SerializeField] private MinigameShapeController minigameShapeController;
+
     public delegate void MinigameOver();
-    public static event MinigameOver OnMinigameOver;
-
-    private MinigameShapeController minigameShapeController;
-
+    public static event MinigameOver OnMinigameOver = delegate { };
     
     // Start is called before the first frame update
     virtual protected void Start()
     { 
-       minigameShapeController = FindObjectOfType<MinigameShapeController>();
        minigameShapeController.SetCanMoveShapes(true);
        secondsLeftToSolve = secondsToSolve;
-       timerBar.fillAmount = 1f; // Sets the timerbar to full
+       // Set the timer bar to full
+       timerBar.fillAmount = 1f;
     }
 
     // Update is called once per frame
     virtual protected void Update()
     {
-        secondsLeftToSolve-= Time.deltaTime;
-        depleteTimerBar();
-        if(secondsLeftToSolve<=0)
+        if (secondsLeftToSolve > 0)
         {
-            if (OnMinigameOver!= null)
+            depleteTimerBar();
+            if (secondsLeftToSolve < 0)
             {
-                OnMinigameOver(); // Trigger MinigameOver Event
+                OnMinigameOver();
+                minigameShapeController.SetCanMoveShapes(false);
+                secondsLeftToSolve = 0;
             }
-            minigameShapeController.SetCanMoveShapes(false);
-            
         }
     }
 
     private void depleteTimerBar()
     {
+        secondsLeftToSolve -= Time.deltaTime;
         timerBar.fillAmount = secondsLeftToSolve / secondsToSolve;
     }
 }
