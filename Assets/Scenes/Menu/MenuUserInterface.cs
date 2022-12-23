0271c440e901436156913013b7fdc925fdebc9ec
Mario Nowak <m.now@protonmail.com>
2022-12-23 22:11:38 +0100

    Extend main menu
    Co-Authored-By: Ramzes Khotambekzoda <ramzeshotambekzofa@gmail.com>

--- Assets/Scenes/Menu/MenuUserInterface.cs
+++ Assets/Scenes/Menu/MenuUserInterface.cs
@@ -4,11 +4,18 @@ using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Unity.Netcode.Transports.UTP;
+using TMPro;
+using UnityEngine.Events;
 
 public class MenuUserInterface : NetworkBehaviour
 {
     [SerializeField] private Button pilotButton;
     [SerializeField] private Button instructorButton;
+    [SerializeField] private Button confirmationButton;
+
+    [SerializeField] private TextMeshProUGUI IpAddressInfoLabel;
+    [SerializeField] private TMP_InputField ipAddressInput;
     [SerializeField] private NetworkObject sharedGameStatePrefab;
 
     void Start()
@@ -28,10 +35,30 @@ public class MenuUserInterface : NetworkBehaviour
             var sceneName = IsHost ? "Scenes/PilotGame" : "Scenes/InstructorGame";
             SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
         };
+        
         pilotButton.onClick.AddListener(() => {
+            string ipAddress = IpAddressManager.GetIpAddress(IpAddressVersion.v4);
+            var transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+            transport.SetConnectionData(ipAddress, 7778);
+            IpAddressInfoLabel.text = $"Your IP address is {ipAddress}\n waiting for the client...";
+
+            instructorButton.gameObject.SetActive(false);
+            pilotButton.gameObject.SetActive(false);
+            IpAddressInfoLabel.gameObject.SetActive(true);
+
             NetworkManager.Singleton.StartHost();
         });
+
         instructorButton.onClick.AddListener(() => {
+            instructorButton.gameObject.SetActive(false);
+            pilotButton.gameObject.SetActive(false);
+            ipAddressInput.gameObject.SetActive(true);
+            confirmationButton.gameObject.SetActive(true);
+        });
+
+        confirmationButton.onClick.AddListener(() => {
+            var transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+            transport.SetConnectionData(ipAddressInput.text, 7778);
             NetworkManager.Singleton.StartClient();
         });
     }
