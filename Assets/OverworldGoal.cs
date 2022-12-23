d3636ad285ccf142ed8b0e9142c0208f52cb0aa1
RhettDuan <57241685+RhettDuan@users.noreply.github.com>
2022-12-23 20:02:19 +0100

    Add an overworld goal (#16)
    * first commit
    
    * Type changed to Spaceship
    
    * Add static event to overworld goal
    
    * Fix overworld goal prefab
    
    * Add tag and collider to overworld spaceship
    
    Co-authored-by: Mario Nowak <m.now@protonmail.com>

---
+++ Assets/OverworldGoal.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverworldGoal : MonoBehaviour
+{
+    public delegate void CollidedWithSpaceship();
+    public static event CollidedWithSpaceship OnCollidedWithSpaceship;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "OverworldSpaceship"){
+            OnCollidedWithSpaceship();
+        }
+    }
+}
