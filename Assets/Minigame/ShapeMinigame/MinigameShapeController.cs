b8c1d9dfc5682d897dccfea4653f23b08eb41859
Mario Nowak <m.now@protonmail.com>
2022-12-25 23:13:26 +0100

    Fix merge issues

--- Assets/Minigame/ShapeMinigame/MinigameShapeController.cs
+++ Assets/Minigame/ShapeMinigame/MinigameShapeController.cs
@@ -10,11 +10,13 @@ public class MinigameShapeController : MonoBehaviour
     private Grid grid;
 
     private bool canMoveShapes;
+    private Camera minigameCamera;
 
     void Start()
     {
         shapes = FindObjectsOfType<MinigameShape>();
         grid = GetComponent<Grid>();
+        minigameCamera = GameObject.FindGameObjectWithTag("MinigameCamera").GetComponent<Camera>();
     }
 
     void Update()
@@ -23,7 +25,7 @@ public class MinigameShapeController : MonoBehaviour
         {
             if (Input.GetMouseButton(0) && currentlyDraggedShape == null)
             {
-                Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Vector2 mousePosition = minigameCamera.ScreenToWorldPoint(Input.mousePosition);
                 foreach (var shape in shapes)
                 {
                     if (shape.hitbox.OverlapPoint(mousePosition))
@@ -52,7 +54,7 @@ public class MinigameShapeController : MonoBehaviour
        {
            if (currentlyDraggedShape != null)
            {
-                Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Vector2 mousePosition = minigameCamera.ScreenToWorldPoint(Input.mousePosition);
                 var newPosition = Snapping.Snap(mousePosition - grabOffset, (Vector2) grid.cellSize);
                 currentlyDraggedShape.Move(to: newPosition);
            }
