3c451d28a991ce1bf8272f7f8f1693980ad20882
Mario Nowak <m.now@protonmail.com>
2023-01-05 16:17:23 +0100

    Let solution book appear when clicked

--- Assets/Scenes/Instructor/Book/ShapeMinigameBook.cs
+++ Assets/Scenes/Instructor/Book/ShapeMinigameBook.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class ShapeMinigameBook : MonoBehaviour
+public class ShapeMinigameBook : MinigameBook
 {
     [SerializeField]
     private ScenarioManager scenarioManagerPrefab;
     private Grid grid;
+
     void Start()
     {
+        Hide();
         grid = transform.Find("Grid").GetComponent<Grid>();
         SharedGameState.OnInstructorReceivedGameState += () => {
             GenerateSolutionExplanation();
@@ -29,5 +31,16 @@ public class ShapeMinigameBook : MonoBehaviour
             var shape = Instantiate(shapePrefab, parent: grid.transform);
             shape.transform.localPosition = shapePosition;
         }
+    }
+
+    public override void Display()
+    {
+        transform.localPosition = Vector3.zero;
+    }
+
+    public override void Hide()
+    {
+        // I'm sorry for this but this is really the simplest solution
+        transform.localPosition = new Vector3(1000, 1000, 1000);
     }
 }
