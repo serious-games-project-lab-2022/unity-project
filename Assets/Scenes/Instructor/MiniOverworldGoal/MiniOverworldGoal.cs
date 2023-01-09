4dc1c48ec58f0e5314d9840296490f164a040853
Marko Bregasi <74657318+Gauteee@users.noreply.github.com>
2023-01-09 15:07:57 +0100

    Send overworldgoal position over network to instructor scene

---
+++ Assets/Scenes/Instructor/MiniOverworldGoal/MiniOverworldGoal.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniOverworldGoal : MonoBehaviour
+{
+    private SharedGameState sharedGameState;
+
+    void Start()
+    {
+        SharedGameState.OnInstructorReceivedGameState += () => {
+            sharedGameState = GameObject.FindObjectOfType<SharedGameState>();
+
+            sharedGameState.overworldGoalPosition.OnValueChanged += (Vector2 preValue, Vector2 newValue) => {
+                transform.localPosition = newValue / 16f;
+            };
+        };
+    }
+}
