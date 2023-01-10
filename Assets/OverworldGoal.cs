0dfa45d36bc20a931b809942627273ae214ffa74
Marko Bregasi <74657318+Gauteee@users.noreply.github.com>
2023-01-10 09:12:26 +0100

    Send overworldgoal position over network to instructor scene (#44)

--- Assets/OverworldGoal.cs
+++ Assets/OverworldGoal.cs
@@ -7,6 +7,18 @@ public class OverworldGoal : MonoBehaviour
     public delegate void CollidedWithSpaceship();
     public static event CollidedWithSpaceship OnCollidedWithSpaceship;
 
+    private SharedGameState sharedGameState;
+
+    void Start()
+    {
+        sharedGameState = GameObject.FindObjectOfType<SharedGameState>();
+       
+        if (sharedGameState != null)
+        {
+            sharedGameState.overworldGoalPosition.Value = new Vector2(transform.localPosition.x, transform.localPosition.y);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "OverworldSpaceship"){
