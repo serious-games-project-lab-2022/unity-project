4e60aad7ac2e641303a239098d353ef6ad43667d
Mario Nowak <m.now@protonmail.com>
2023-03-13 19:31:03 +0100

    Add knockback and hitstun to the spaceship

--- Assets/Scenes/Pilot/Spaceship/Spaceship.cs
+++ Assets/Scenes/Pilot/Spaceship/Spaceship.cs
@@ -7,7 +7,18 @@ public class Spaceship : MonoBehaviour
 {
     public delegate void CollidedWithTerrain();
     public event CollidedWithTerrain OnCollidedWithTerrain = delegate {};
+    private Rigidbody2D rigidbody2D;
+    private Animation blinkingAnimation;
+    public float knockBackStrength = 3;
+    public float hitStunDurationInSeconds = 2;
+    private bool inHitStun = false;
+    private float hitStunTimer = 0;
 
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        blinkingAnimation = GetComponent<Animation>();
+    }
 
     void Update()
     {
@@ -17,12 +28,33 @@ public class Spaceship : MonoBehaviour
             sharedGameState.spaceshipPosition.Value = new Vector2(transform.localPosition.x, transform.localPosition.y);
             sharedGameState.spaceshipRotation.Value = transform.eulerAngles.z;
         }
+
+        if (inHitStun)
+        {
+            hitStunTimer += Time.fixedDeltaTime;
+            if (hitStunTimer >= hitStunDurationInSeconds)
+            {
+                hitStunTimer = 0;
+                inHitStun = false;
+            }
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "OverworldTerrain")
         {
+            if (inHitStun)
+            {
+                return;
+            }
+            var collisionPoint = collision.GetContact(0).point;
+            var knockBackDirection = (rigidbody2D.position - collisionPoint).normalized;
+
+            rigidbody2D.AddForce(knockBackDirection * knockBackStrength, ForceMode2D.Impulse);
+            blinkingAnimation.Play();
+            inHitStun = true;
+
             OnCollidedWithTerrain();
         }
     }
