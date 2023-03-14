195f5726c50e4c80f11334516a20733ae91e3423
Mario Nowak <48293574+mario-nowak@users.noreply.github.com>
2023-03-14 19:42:22 +0100

    Task/improve-game-effects (#105)
    * Added Screen Shaker
    
    * Added Screen Shaker in case of SpaceShip Collision
    
    * + Collision Noice // SpaceShip Motor
    
    * Added SpaceShip Engine Sound
    
    * Added SpaceShip Particles
    
    * Changed SpaceShip emission
    
    * Added camera Shaker
    
    * Added Collision Sound
    
    * changed space ship polygon collider shape
    
    * Added Fire Emission
    
    => later on I will add more sub versions of fire
    
    * Deleted not needed skripts
    
    * Deleted not needed scripts
    
    * Add sound on collision with terrain
    
    * Change sky background
    
    * Add sounds when spawning and removing minigames
    
    * Add fuel bar
    
    * Reposition back button
    
    * Adjust main menu
    
    * Improve menu
    
    * Improve ready buttons
    
    * Adjust menu
    
    * Fix sounds
    
    * Change colors
    
    ---------
    
    Co-authored-by: Ramzes Khotambekzoda <ramzeshotambekzofa@gmail.com>

---
+++ Assets/Scenes/Pilot/GameEffects/CameraShaker.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShaker : MonoBehaviour
+{
+    public float duration = 1f;
+    public bool shake = false;
+    public AnimationCurve curve;
+   /* void Update()
+    {
+        if(shake)
+        {
+            shake = false;
+            StartCoroutine(Shaking());
+        }
+    }*/
+
+    private IEnumerator Shaking()
+    {
+        Vector3 startPosition = transform.position;
+        float elapsedTime = 0f;
+
+        while(elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float strength = curve.Evaluate(elapsedTime / duration);
+            transform.position = startPosition + Random.insideUnitSphere * strength;
+            yield return null;
+        }
+        transform.position = startPosition;
+    }
+
+
+    public void shakePilot()
+    {
+        StartCoroutine(Shaking());
+    }
+}
